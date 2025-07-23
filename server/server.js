468e217e6fa161f4c4bf693fea51211f9f8b3ee9
sudeepfr <sudeepverma111213@gmail.com>
2025-07-23 11:28:40 +0530

    fixed vecel problem

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/mongodb.js';
-
+  
 
 const PORT =process.env.PORT ||4000;
 const app=express();
