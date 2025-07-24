b61c4709d511054fc949e3c5538f05dd6bcb5307
sudeepfr <sudeepverma111213@gmail.com>
2025-07-24 08:36:10 +0530

    mongoo fixed 4

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,7 +4,7 @@ import userModel from "../models/userModel.js";
 
 const clerkWebhooks = async (req, res) => {
     try {
-        console.log("🔥 Webhook hit!");
+        console.log("Webhook hit!");
         //create svix instance with clerk webhook secret
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
 
@@ -18,37 +18,36 @@ const clerkWebhooks = async (req, res) => {
         const evt = whook.verify(payload, headers);
 
         const { data, type } = evt;
-        console.log("📦 Webhook type:", type);
+        console.log(" Webhook type:", type);
         switch (type) {
             case "user.created": {
                 const userData = {
                     clerkId: data.id,
-                    email: data.email_addresses?.[0]?.email_address || "", // safe fallback
+                    email: data.email_addresses?.[0]?.email_address || "",
                     photo: data.image_url,
-                    firstName: data.first_name,
-                    lastName:data.last_name||'',
+                    firstName: data.first_name || '',
+                    lastName: data.last_name || '',
                 };
                 const existingUser = await userModel.findOne({ clerkId: data.id });
                 if (existingUser) {
-                    console.log("👀 User already exists:", existingUser.email);
+                    console.log("User already exists:", existingUser.email);
                     break;
                 }
                 try {
                     await userModel.create(userData);
-                    console.log("🧑 User created successfully");
+                    console.log(" User created successfully");
                 } catch (err) {
-                    console.error("❌ Error creating user:", err.message);
+                    console.error(" Error creating user:", err.message);
                 }
                 break;
 
             }
             case "user.updated": {
-                  const userData = {
-                    email: data.email_addresses?.[0]?.email_address || "", // safe fallback
+                const userData = {
+                    email: data.email_addresses?.[0]?.email_address || "",
                     photo: data.image_url,
-                    firstName: data.first_name,
-                    lastName:data.last_name||'',
-                 
+                    firstName: data.first_name || '',
+                    lastName: data.last_name || '',   
                 };
                 await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
                 res.json({});
